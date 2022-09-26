f990255eefb4a4eedc70f5a9196c8a2a869376d9
Edoardo Nervo <edoardonervo96@gmail.com>
2022-09-26 21:56:38 +0200

    Add GHG emission database, init main.ipynb

--- main.py
+++
@@ -1,9 +0,0 @@
-from utils import cleaning, config
-
-def main():
-    # Load data
-    data_raw = cleaning.load_data(config.DB_PATH)
-    print(data_raw.head())
-
-if __name__ == '__main__':
-    main()
